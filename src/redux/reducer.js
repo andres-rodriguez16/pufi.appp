2259ee7dbd7a058c90e7ef0a650c224722a25841
andresyrg16 <andresyrg16@gmail.com>
2022-10-08 16:05:35 -0500

    configuracion de redux

---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+  moviesFavourites: [],
+  moviesLoaded: [],
+  movieDetail: {},
+};
+
+function rootReducer(state = initialState, action) {
+  
+  return state;
+}
+
+export default rootReducer;
---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { applyMiddleware, compose } from "redux";
+import { createStore } from "redux";
+import rootReducer from "../redux/reducer";
+import thunk from "redux-thunk";
+
+
+const composeEnhancers =
+	(typeof window !== 'undefined' &&
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+	compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+
+);
+
+export default store;
