549adcbd4513ae3f19c6e31e6fc0e2e88474a58b
andresyrg16 <andresyrg16@gmail.com>
2022-10-09 18:04:51 -0500

    configuracion de sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import s from './App.module.css';
+import s from '././Style/App.scss';
 import Navbar from './components/Navbar/Navbar.jsx';
-
+import Carousel from './components/Carousel/Carousel';
 function App() {
   return (
     <div className={s.app}>
       <Navbar/>
+      {/* <Carousel/> */}
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import s from './Navbar.module.css';
 import image from '../../images/logo.png';
 import Navigation from './Navigation/Navigation';
 import MyProfile from './myProfile/MyProfile';
 function Navbar() {
   return (
-    <div className={s.navBar}>
-      <div className={s.logo}>
-        <img className={s.logo__image} src={image} alt='logoPufi' />
+    <div className="navBar">
+      <div className="logo">
+        <img className="logo__image" src={image} alt='logoPufi' />
       </div>
-      <div className={s.Navigation}>
+      <div className="Navigation">
         <Navigation />
       </div>
       <dir>
