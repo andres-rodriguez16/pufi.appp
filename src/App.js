f42484d159e975e7f0ad480ae5e66ff261b01ef0
andresyrg16 <andresyrg16@gmail.com>
2022-10-10 16:15:38 -0500

    imaganes lista y tutulos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import Navbar from './components/Navbar/Navbar.jsx';
 import Footer from './components/Footer/Footer';
 import Form from './components/Form/Form.jsx';
-import Suscribite from './components/Form/Suscribite.jsx';
+import Carousel from './components/Carousel/Carousel.jsx';
 
 function App() {
   return (
     <div className="app">
-      <Navbar/>
-      {/* <Carousel/> */}
-      <Suscribite/>
-     <Form/>
-      <Footer/>
+      <div>
+        <Navbar />
+        <Carousel />
+      </div>
+      <Form />
+      <Footer />
     </div>
   );
 }
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
 import s from './Carousel.module.css';
-import carousel1 from '../../images/Carousel1.jpg'
-import carousel2 from '../../images/carousel2.jpg'
+import carousel4 from '../../images/carousel4.jpg';
+import carousel1 from '../../images/Carousel1.jpg';
 
 function Carousel() {
   return (
-    <div className={s.slider}>
+    <div className={s.container}>
       <ul>
         <li>
-          <img src={carousel1} alt="image1" />
-        </li>
-        <li>
-          <img src={carousel2} alt="image2" />
+          <img src={carousel4} alt='' />
+          <h1 className={s.titulo}>ESTAR CÓMODO NUNCA 
+          <br/>FUE TAN FÁCIL</h1>
+          <button>SHOP</button>
         </li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
